e05f2eae2f5ce5caf41dd1dc0c7dc406fce68246
ce7elem <olivier.benaben@protonmail.com>
2023-08-31 19:16:07 +0200

    wip

--- src/executor.rs
+++ src/executor.rs
@@ -1,6 +1,25 @@
+use std::process::{Command, Stdio};
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+
 pub struct DefaultExecutor {}
 impl DefaultExecutor {
-    pub fn run(&self, _command: Vec<String>) -> bool {
-        return true;
+    pub fn run(&self, _command: Vec<String>) -> Result<(), Error> {
+
+    fn main() -> Result<(), Error> {
+        let stdout = Command::new("journalctl")
+            .stdout(Stdio::piped())
+            .spawn()?
+            .stdout
+            .ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))?;
+
+        let reader = BufReader::new(stdout);
+
+        reader
+            .lines()
+            .filter_map(|line| line.ok())
+            .filter(|line| line.find("usb").is_some())
+            .for_each(|line| println!("{}", line));
+
+         Ok(())
     }
 }

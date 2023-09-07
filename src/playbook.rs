0f2c5b6a0f3c4ff3ed00204e603384bb3791df30
ce7elem <olivier.benaben@protonmail.com>
2023-09-07 22:15:58 +0200

    refactor: improve API by taking String instead of &str
    BREAKING CHANGE: Core types are changing.

--- src/playbook.rs
+++ src/playbook.rs
@@ -7,29 +7,28 @@ use std::process::Child;
 /// Parameters described on `Options` section within
 /// ansible-playbook's man page, and which defines which should be
 /// the ansible-playbook execution behavior.
-#[derive(Clone)]
-pub struct AnsiblePlaybookOptions<'a> {
+pub struct AnsiblePlaybookOptions {
     pub ask_vault_password: bool,      // ask for vault password
     pub check: bool, // don't make any changes; instead, try to predict some of the changes that may occur
     pub diff: bool, // when changing (small) files and templates, show the differences in those files; works great with --check
     pub extra_vars: serde_json::Value, // is a map of extra variables used on ansible-playbook execution
-    pub extra_vars_file: Vec<&'a str>, // is a list of files used to load extra-vars
+    pub extra_vars_file: Vec<String>, // is a list of files used to load extra-vars
     pub flush_cache: bool,             // is the flush cache flag for ansible-playbook
     pub force_handlers: bool,          // run handlers even if a task fails
-    pub forks: &'a str,                // specify number of parallel processes to use (default=50)
-    pub inventory: &'a str,            // specify inventory host path
-    pub limit: &'a str,                // is selected hosts additional pattern
+    pub forks: String,                // specify number of parallel processes to use (default=50)
+    pub inventory: String,            // specify inventory host path
+    pub limit: String,                // is selected hosts additional pattern
     pub list_hosts: bool,              // outputs a list of matching hosts
     pub list_tags: bool,               // is the list tags flag for ansible-playbook
     pub list_tasks: bool,              // is the list tasks flag for ansible-playbook
-    pub module_path: &'a str, // repend colon-separated path(s) to module library (default=~/.ansible/plugins/modules:/usr/share/ansible/plugins/modules)
-    pub skip_tags: &'a str,   // only run plays and tasks whose tags do not match these values
-    pub start_at_task: &'a str, // start the playbook at the task matching this name
+    pub module_path: String, // repend colon-separated path(s) to module library (default=~/.ansible/plugins/modules:/usr/share/ansible/plugins/modules)
+    pub skip_tags: String,   // only run plays and tasks whose tags do not match these values
+    pub start_at_task: String, // start the playbook at the task matching this name
     pub step: bool,           // one-step-at-a-time: confirm each task before running
     pub syntax_check: bool,   // is the syntax check flag for ansible-playbook
-    pub tags: &'a str,        // is the tags flag for ansible-playbook
-    pub vault_id: &'a str,    // the vault identity to use
-    pub vault_password_file: &'a str, // path to the file holding vault decryption key
+    pub tags: String,        // is the tags flag for ansible-playbook
+    pub vault_id: String,    // the vault identity to use
+    pub vault_password_file: String, // path to the file holding vault decryption key
     pub verbose: bool,        // verbose mode enabled
     pub verbose_v: bool,      // verbose mode -v enabled
     pub verbose_vv: bool,     // verbose mode -vv enabled
@@ -38,7 +37,7 @@ pub struct AnsiblePlaybookOptions<'a> {
     pub version: bool, // show program's version number, config file location, configured module search path, module location, executable location and exit
 }
 
-impl Default for AnsiblePlaybookOptions<'_> {
+impl Default for AnsiblePlaybookOptions {
     fn default() -> Self {
         AnsiblePlaybookOptions {
             ask_vault_password: false,
@@ -48,20 +47,20 @@ impl Default for AnsiblePlaybookOptions<'_> {
             extra_vars_file: vec![],
             flush_cache: false,
             force_handlers: false,
-            forks: "",
-            inventory: "",
-            limit: "",
+            forks: String::new(),
+            inventory:  String::new(), 
+            limit: String::new(),
             list_hosts: false,
             list_tags: false,
             list_tasks: false,
-            module_path: "",
-            skip_tags: "",
-            start_at_task: "",
+            module_path: String::new(),
+            skip_tags: String::new(),
+            start_at_task: String::new(),
             step: false,
             syntax_check: false,
-            tags: "",
-            vault_id: "",
-            vault_password_file: "",
+            tags: String::new(),
+            vault_id: String::new(),
+            vault_password_file: String::new(),
             verbose: false,
             verbose_v: false,
             verbose_vv: false,
@@ -72,7 +71,7 @@ impl Default for AnsiblePlaybookOptions<'_> {
     }
 }
 
-impl AnsiblePlaybookOptions<'_> {
+impl AnsiblePlaybookOptions {
     const ASK_VAULT_PASSWORD_FLAG: &str = "--ask-vault-password";
     const CHECK_FLAG: &str = "--check";
     const DIFF_FLAG: &str = "--diff";
@@ -159,17 +158,17 @@ impl AnsiblePlaybookOptions<'_> {
 
         if !self.forks.is_empty() {
             cmd.push(Self::FORKS_FLAG.to_string());
-            cmd.push(self.forks.to_string().clone());
+            cmd.push(self.forks.clone());
         }
 
         if !self.inventory.is_empty() {
             cmd.push(Self::INVENTORY_FLAG.to_string());
-            cmd.push(self.inventory.to_string().clone());
+            cmd.push(self.inventory.clone());
         }
 
         if !self.limit.is_empty() {
             cmd.push(Self::LIMIT_FLAG.to_string());
-            cmd.push(self.limit.to_string().clone());
+            cmd.push(self.limit.clone());
         }
 
         if self.list_hosts {
@@ -186,17 +185,17 @@ impl AnsiblePlaybookOptions<'_> {
 
         if !self.module_path.is_empty() {
             cmd.push(Self::MODULE_PATH_FLAG.to_string());
-            cmd.push(self.module_path.to_string().clone());
+            cmd.push(self.module_path.clone());
         }
 
         if !self.skip_tags.is_empty() {
             cmd.push(Self::SKIP_TAGS_FLAG.to_string());
-            cmd.push(self.skip_tags.to_string().clone());
+            cmd.push(self.skip_tags.clone());
         }
 
         if !self.start_at_task.is_empty() {
             cmd.push(Self::START_AT_TASK_FLAG.to_string());
-            cmd.push(self.start_at_task.to_string().clone());
+            cmd.push(self.start_at_task.clone());
         }
 
         if self.step {
@@ -209,12 +208,12 @@ impl AnsiblePlaybookOptions<'_> {
 
         if !self.tags.is_empty() {
             cmd.push(Self::TAGS_FLAG.to_string());
-            cmd.push(self.tags.to_string().clone());
+            cmd.push(self.tags.clone());
         }
 
         if !self.vault_id.is_empty() {
             cmd.push(Self::VAULT_ID_FLAG.to_string());
-            cmd.push(self.vault_id.to_string().clone());
+            cmd.push(self.vault_id.clone());
         }
 
         if !self.vault_password_file.is_empty() {
@@ -236,21 +235,20 @@ impl AnsiblePlaybookOptions<'_> {
 }
 
 /// Ansible-playbook command representation and how to execute it
-#[derive(Clone)]
-pub struct AnsiblePlaybookCmd<'a> {
-    pub binary: &'a str,                                  // Ansible binary
+pub struct AnsiblePlaybookCmd {
+    pub binary: String,                                  // Ansible binary
     pub executor: DefaultExecutor,                        // Ansible binary
-    pub playbooks: Vec<&'a str>,                          // playbooks list to be run
-    pub options: AnsiblePlaybookOptions<'a>,              // playbook options
-    pub connection_options: AnsibleConnectionOptions<'a>, // specific options for connection
-    pub privilege_escalation_options: AnsiblePrivilegeEscalationOptions<'a>, // playbook's privilege escalation options
+    pub playbooks: Vec<String>,                          // playbooks list to be run
+    pub options: AnsiblePlaybookOptions,              // playbook options
+    pub connection_options: AnsibleConnectionOptions, // specific options for connection
+    pub privilege_escalation_options: AnsiblePrivilegeEscalationOptions, // playbook's privilege escalation options
 }
 
 const DEFAULT_ANSIBLE_PLAYBOOK_BINARY: &str = "ansible-playbook";
-impl Default for AnsiblePlaybookCmd<'_> {
+impl Default for AnsiblePlaybookCmd {
     fn default() -> Self {
         AnsiblePlaybookCmd {
-            binary: DEFAULT_ANSIBLE_PLAYBOOK_BINARY,
+            binary: DEFAULT_ANSIBLE_PLAYBOOK_BINARY.into(),
             executor: DefaultExecutor {},
             playbooks: vec![],
             options: AnsiblePlaybookOptions {
@@ -266,10 +264,10 @@ impl Default for AnsiblePlaybookCmd<'_> {
     }
 }
 
-impl AnsiblePlaybookCmd<'_> {
+impl AnsiblePlaybookCmd {
     /// run playbooks
     pub fn run(&self) -> Result<Child, Box<dyn Error + '_>> {
-        if let Err(err) = verify_binary(self.binary) {
+        if let Err(err) = verify_binary(&self.binary) {
             return Err(format!("(playbook::run) {}", err).into());
         }
 
@@ -288,7 +286,7 @@ impl AnsiblePlaybookCmd<'_> {
     pub fn command(&self) -> Result<Vec<String>, Box<dyn Error>> {
         let mut cmd = vec![];
 
-        cmd.push(self.binary.to_string().clone());
+        cmd.push(self.binary.clone());
 
         cmd.append(
             &mut self
@@ -308,7 +306,7 @@ impl AnsiblePlaybookCmd<'_> {
                 .gen_cmd_privesc_opts()
                 .expect("(playbook::command) Generate privilige escalation options"),
         );
-        cmd.append(&mut self.playbooks.iter().map(|&s| s.into()).collect());
+        cmd.append(&mut self.playbooks.clone());
 
         return Ok(cmd);
     }
